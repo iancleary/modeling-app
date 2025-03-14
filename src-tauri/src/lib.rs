30a78c80f4be163e4cefd7401abd0e915189680b
Ian Cleary <github@iancleary.me>
2025-03-13 21:45:27 -0700

    Bump version, add ; to macos build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -35,7 +35,7 @@ pub fn run() {
 
 				// Set window level
 				// NSWindowLevel: https://developer.apple.com/documentation/appkit/nswindowlevel
-				main_window.set_window_level(25).unwrap()
+				main_window.set_window_level(25).unwrap();
 			}
 
 			Ok(())
