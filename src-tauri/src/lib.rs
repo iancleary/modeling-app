4ac75f05f58c91ca2c7c378cea89d033d91ada6c
Ian Cleary <github@iancleary.me>
2025-03-13 21:23:42 -0700

    Add tauri-plugin-decorum

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,9 @@
 mod commands;
+use tauri::Manager;
+
+use tauri_plugin_decorum::WebviewWindowExt; // adds helper methods to WebviewWindow
+
+
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -6,10 +11,35 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_decorum::init())
+        .setup(|app| {
+			// Create a custom titlebar for main window
+			// On Windows this hides decoration and creates custom window controls
+			// On macOS it needs hiddenTitle: true and titleBarStyle: overlay
+			let main_window = app.get_webview_window("main").unwrap();
+			main_window.create_overlay_titlebar().unwrap();
+
+			// Some macOS-specific helpers
+			#[cfg(target_os = "macos")] {
+				// Set a custom inset to the traffic lights
+				main_window.set_traffic_lights_inset(12.0, 16.0).unwrap();
+
+				// Make window transparent without privateApi
+				main_window.make_transparent().unwrap();
+
+				// Set window level
+				// NSWindowLevel: https://developer.apple.com/documentation/appkit/nswindowlevel
+				main_window.set_window_level(25).unwrap()
+			}
+
+			Ok(())
+		})
         .invoke_handler(tauri::generate_handler![greet, commands::show_main_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
